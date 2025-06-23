cf10851cbff29aa5d048c6829b3a1fde8c65be47
Miftahush Syaadah <miftahussyaadah12@gmail.com>
2025-06-23 22:57:23 +0800

    Inisialisasi struktur data BakeryItem dan pointer head

---
+++ inv-Bakery.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct BakeryItem {
+    string nama;
+    int harga;
+    int stok;
+    BakeryItem* next;
+};
+
+BakeryItem* head = nullptr;
+
+
